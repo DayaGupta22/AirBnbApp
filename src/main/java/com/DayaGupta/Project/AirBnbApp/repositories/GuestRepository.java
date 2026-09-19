0111f46dc746dd6f56719156976a615aa092b2cf
daya <daya@code.com>
2026-09-19 19:46:36 +0530

    Booking Service Updating continuesouly..

---
+++ src/main/java/com/DayaGupta/Project/AirBnbApp/repositories/GuestRepository.java
@@ -0,0 +1,4 @@
+package com.DayaGupta.Project.AirBnbApp.repositories;
+
+public interface GuestRepository extends org.springframework.data.jpa.repository.JpaRepository<com.DayaGupta.Project.AirBnbApp.entities.Guest, java.lang.Long> {
+}
